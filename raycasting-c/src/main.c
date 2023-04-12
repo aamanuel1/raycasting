3e589d2746d2033c9b5e297da3ce567a76073374
Ubuntu <aaron.manuel@ucalgary.ca>
2023-04-12 15:57:50 +0000

    Finished up to polling tutorial

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -1,6 +1,89 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "constants.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int isGameRunning = FALSE;
+
+int initializeWindow(){
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		fprintf(stderr, "Error initializing SDL.\n");
+		return FALSE;
+	}
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+	if(!window){
+	fprintf(stderr, "Error creating SDL window.\n");
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0); //1 is get default driver
+	if(!renderer){
+	fprintf(stderr, "Error creating SDL renderer.\n");
+	return FALSE;
+	}
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND); //how to blend texture
+	return TRUE;
+}
+
+void destroyWindow(){
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void setup(){
+	//TODO
+	//initialize and setup game objects
+}
+
+void processInput(){
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch(event.type){
+		case SDL_QUIT:{
+			isGameRunning = FALSE;
+			break;
+		}
+		case SDL_KEYDOWN:{
+		if(event.key.keysym.sym == SDLK_ESCAPE)
+			isGameRunning = FALSE;
+		break;
+		}	
+	}
+}
+
+void render(){
+	
+	//clear the frame
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);	
+	
+	//TODO:
+	//render all game objects for the current frame
+	
+	//swap the buffer
+	SDL_RenderPresent(renderer);
+}
 
 int main(){
-	puts("Program is running.\n");
+	isGameRunning = initializeWindow();
+
+	setup();
+
+	while(isGameRunning){
+		processInput();
+		//update();
+		//render();
+	}
+	
+	destroyWindow();
+
 	return 0;
 }
